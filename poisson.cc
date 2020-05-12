651ce64ee79bafc255900318b2a5c5cbc2e28198
JasminBecks <jbecks@physnet.uni-hamburg.de>
2020-05-12 08:12:41 +0000

    Nummer 1a und erster Teil b

--- poisson.cc
+++ poisson.cc
@@ -1,20 +1,30 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-
+using namespace std;
 
 double poisson(double mu, int k) {
     return 0;
 }
 
 int main() {
-    using namespace std;
 
 
     ifstream fin("datensumme.txt");
+    ofstream fout("hist.txt");
+    vector<int>zaehler(11); 
     int n_i;
+
     for(int i = 0 ; i < 234 ; ++i) {
         fin >> n_i;
+        zaehler[n_i] += 1;
     }
+    
+    for(int j = 0; j < zaehler.size(); ++j) {
+    fout << j << " " << zaehler[j] << endl;
+    }
+    
     fin.close();
+
+  
 }

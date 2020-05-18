3b5603bdad5d14309ddea05055296eaab0a78d6b
JasminBecks <jbecks@physnet.uni-hamburg.de>
2020-05-18 14:47:20 +0000

    erster Versuch a

--- likelihood.cc
+++ likelihood.cc
@@ -1,10 +1,21 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-
+#include <cmath>
 
 double poisson(double mu, int k) {
-    return 0;
+  using namespace std;
+  double P = (exp(-mu) * pow(mu, k))/(tgamma(k + 1));
+    return P;
+}
+
+double prob(std::vector<int> daten, double mu) {
+  using namespace std;
+  double P = 1;
+  for(int k : daten) {
+    P *= poisson(mu, k);
+  }
+  return P;
 }
 
 int main() {
@@ -12,9 +23,15 @@ int main() {
 
 
     ifstream fin("datensumme.txt");
+
+    vector<int> daten(0);
+
     int n_i;
     for(int i = 0 ; i < 234 ; ++i) {
         fin >> n_i;
+        daten.push_back(n_i);
     }
     fin.close();
+
+    cout << prob(daten , 3.11538) << endl;
 }

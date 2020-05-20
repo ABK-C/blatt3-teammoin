2c9923d0a7512f2f82d918bb7c10dd6657cce7cd
JasminBecks <jbecks@physnet.uni-hamburg.de>
2020-05-20 08:11:30 +0000

    zweiter Versuch

--- likelihood.cc
+++ likelihood.cc
@@ -3,15 +3,15 @@
 #include <vector>
 #include <cmath>
 
-double poisson(double mu, int k) {
+long double poisson(double mu, int k) {
   using namespace std;
-  double P = (exp(-mu) * pow(mu, k))/(tgamma(k + 1));
-    return P;
+  long double P = (exp(-mu) * pow(mu, k))/(tgamma(k + 1));
+  return P;
 }
 
-double prob(std::vector<int> daten, double mu) {
+long double prob(std::vector<int> daten, double mu) {
   using namespace std;
-  double P = 1;
+  long double P = 1;
   for(int k : daten) {
     P *= poisson(mu, k);
   }
@@ -23,15 +23,33 @@ int main() {
 
 
     ifstream fin("datensumme.txt");
+    ofstream lin("likelihood.txt");
+    ofstream nll("nll.txt");
+    ofstream anll("deltanll.txt");
+    
 
     vector<int> daten(0);
 
     int n_i;
     for(int i = 0 ; i < 234 ; ++i) {
-        fin >> n_i;
-        daten.push_back(n_i);
+      fin >> n_i;
+      daten.push_back(n_i);
     }
+
+    for(double i = 0; i <= 6; i += 0.02) {
+      lin << i << " " << prob(daten, i) << endl;
+      nll << i << " " << -2*log(prob(daten, i)) << endl;
+      anll << i << " " << -2*log(prob(daten, i)) + 2*log(prob(daten, 3.11538)) << endl;
+    }  
+
+    
     fin.close();
+    lin.close();
+    nll.close();
 
     cout << prob(daten , 3.11538) << endl;
+
+   //bester mu-Wert bei 2b und d: ca. 3.115
+   //Intervall mit mu<1 ca. (3.001-3.235)
+   //also: mu=3.115 +0.12 -0.114
 }
